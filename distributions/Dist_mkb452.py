cd749e7e3cafb6e4c904620fd7936d9fdee6535d
mariaacunamatata <64418870+mariaacunamatata@users.noreply.github.com>
2020-05-17 15:51:17 -0400

    Final Project

--- distributions/Dist_mkb452.py
+++ distributions/Dist_mkb452.py
@@ -1,23 +1,24 @@
 import numpy as np
 from .base_distribution import BaseDistribution
 
-class Dist_kc90(BaseDistribution):
+class Dist_mkb452(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 13.5
+		self.x_min = 2
+		self.x_max = 2.4
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		y = (1/0.000022737)
+		return (y*np.exp((np.power(-x,3)))*np.sin(x))
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 2.06619
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return 0.06436
 
 
--- distributions/Dist_yx1796.py
+++ distributions/Dist_yx1796.py
@@ -1,23 +1,23 @@
 import numpy as np
 from .base_distribution import BaseDistribution
 
-class Dist_kc90(BaseDistribution):
+class Dist_yx1796(BaseDistribution):
 	def __init__(self):
-		self.f_max = 1
-		self.x_min = -1
-		self.x_max = 1
+		self.f_max = 64/60
+		self.x_min = 2
+		self.x_max = 4
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (x**3)/60
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 3.3
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(0.266)
 
 
